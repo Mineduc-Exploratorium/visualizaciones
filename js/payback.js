7527fecf0990428221a0b612a06367477e9de2aa
Ernesto Laval <ernesto.laval@gmail.com>
2013-04-29 16:31:52 -0400

    Reorganización de vistas  en payback
    Se crea una vista principal que llama a vista de visualización

--- js/payback.js
+++ js/payback.js
@@ -1,12 +1,10 @@
 $().ready(function() {
-	console.log("ready");
 	vista = new VistaPrincipal({el:"#mainchart"});
 });
 
-
 var VistaPrincipal = Backbone.View.extend(
 /** @lends VistaPrincipal.prototype */
-	{
+{
 
 	/**
 	* @class VistaPrincipal vista que despliega visualizacion de ingresos vs costos de carreras
@@ -19,36 +17,11 @@ var VistaPrincipal = Backbone.View.extend(
 	* 
 	* VistaPrincipal Inicia parametros de configuración y llamada a datos
 	*/
-	initialize: function() {
-		// Binding de this (esta vista) al contexto de las funciones indicadas
-		_.bindAll(this,"render", "tootipMessage", "seleccionTipoIE", "seleccionArea")
+	initialize : function() {
+    	// Auxiliar para referirse a this al interior de callback functions
+    	var self = this
 
-		// Alias a this para ser utilizado en callback functions
-		var self = this; 
-
-		// Configuración de espacio de despliegue de contenido en pantalla
-		this.margin = {top: 20, right: 20, bottom: 30, left: 200},
-    	this.width = 1000 - this.margin.left - this.margin.right,
-    	this.height = 400 - this.margin.top - this.margin.bottom;
-
-		
-		// Parámetros utilizados en el Gráfico
-		this.attrx = "costo_estimado";  // Atributo de eje X
-		this.attry = "ingreso_agno4";	// Atributo en eje Y
-		this.attrsize = "";				// Atributo en base al cual se calcula el radio de cada nodo (no)
- 		this.attrcolor = "tipo_institucion_nivel_1";
-  		this.attrTipoIE = this.attrcolor;	// Atributo en base al cual define color de los nodos
-  		this.radious = 10;  			// Radio único utilizado por los nodos
-
-  		// Etiquetas utilizadas en ejes X e Y
-		this.etiquetas = {
-			"ingreso_agno4": "Ingreso al 4º año",
-			"duracion": "Duración (años)",
-			"arancel" : "Arancel (millones $)",
-			"costo_estimado" : "Costo estimado (arancel x duracion)"
-		};
-
-    	// Carga de datos
+		// Carga de datos
     	this.vistaLoading = new VistaLoading({el:this.el});
 		this.vistaLoading.show();
 		d3.tsv("data/empleabilidad2.txt", function(data) {
@@ -57,6 +30,210 @@ var VistaPrincipal = Backbone.View.extend(
 			self.data = data;
 			self.render();
 		});
+	},
+
+	/**
+	* Despliegue inicial de elementos gráficos.
+	*/
+	render : function() {
+
+		// Genera contenedores de elementos (paneles, gráfico, ...)
+		// ========================================================
+		
+		// Contenedor del panel de áreas
+		var elSelectorAreas = d3.select(this.el).append("div").attr("id", "panelSelectorAreas")[0][0];
+
+		// SVG - contenedor principal de elementos visuales
+		this.svg = d3.select(this.el).append("svg")
+
+		// Contenedor del panel de tipos
+		var elSelectorTipoIEs = d3.select(this.el).append("div").attr("id", "panelSelectorTipos")[0][0];
+
+
+		// Vista con tooltip para mostrar ficha de establecimiento
+		this.tooltip = new VistaToolTip();
+
+		// Genera nueva vista que  despliega visualización
+		this.vizView = new VistaVizPayback({
+			svg: this.svg,
+			data: this.data,
+			tooltip : this.tooltip
+		});
+
+		// Cea un nuevo panel para seleccionar tipos de IEs ("Universidades", "Institutos Profesionales", ...)
+    	// -------------------------------------------------
+		// Genera contenido del panel y escucha a evento con tipo seleccionado
+		this.vistaSelectorTipoIE= new VistaPanelTipoIE({el:elSelectorTipoIEs, colorScale: this.vizView.getColorScale()});
+		this.vistaSelectorTipoIE.on("seleccionTipoIE", this.vizView.seleccionTipoIE);
+
+
+		// Crea un nuevo panel para seleccionar áreas
+		// -----------------------------------------
+		// Genera el contenido del panel y escucha a evento con area seleccionada
+		this.vistaSelectorAreas = new VistaPanelSelectorAreas({el: "#panelSelectorAreas", areas: this.vizView.areas})
+		this.vistaSelectorAreas.on("seleccionArea", this.vizView.seleccionArea);
+
+
+	}
+});
+
+
+var VistaVizPayback = Backbone.View.extend(
+/** @lends VistaVizPayback.prototype */
+	{
+
+	/**
+	* @class VistaPrincipal vista que despliega visualizacion de ingresos vs costos de carreras
+	*
+	* @augments Backbone.View
+	* @constructs
+	*
+	* @param {object} options parametros de incializacion
+	* @param {array} options.data arreglo con datos (cada dato es un objeto con atributos)
+	* @param {d3.select()} options.svg elemento SVG utilizado como contenedor del gráfico
+	* @param {Backbone.View} options.tooltip vista utilizada como tooltip
+	* VistaVizPayback Inicia parametros de configuración y llamada a datos
+	*/
+	initialize: function() {
+		this.svg = this.options && this.options.svg ? this.options.svg : document.createElementNS('http://www.w3.org/2000/svg', "g");
+		this.data = this.options && this.options.data ? this.options.data : [];
+		this.tooltip = this.options && this.options.tooltip ? this.options.tooltip : new VistaToolTip();
+
+		// Binding de this (esta vista) al contexto de las funciones indicadas
+		_.bindAll(this,"render", "tootipMessage", "seleccionTipoIE", "seleccionArea")
+
+		// Alias a this para ser utilizado en callback functions
+		var self = this; 
+		
+		// Configuración de espacio de despliegue de contenido en pantalla
+		this.margin = {top: 20, right: 20, bottom: 30, left: 200},
+    	this.width = 1000 - this.margin.left - this.margin.right,
+    	this.height = 400 - this.margin.top - this.margin.bottom;
+
+		// Parámetros utilizados en el Gráfico
+		this.attrx = "costo_estimado";  // Atributo de eje X
+		this.attry = "ingreso_agno4";	// Atributo en eje Y
+		this.attrcolor = "tipo_institucion_nivel_1"; // Atributo para selección de color en puntos
+		this.attrfilter = "area"; // Atributo para filtro de datos
+ 		this.radious = 10; 			// Radio único utilizado por los nodos
+
+ 		// Etiquetas utilizadas en ejes X e Y
+		this.etiquetas = {
+			"ingreso_agno4": "Ingreso al 4º año",
+			"duracion": "Duración (años)",
+			"arancel" : "Arancel (millones $)",
+			"costo_estimado" : "Costo estimado (arancel x duracion)"
+		};
+
+		this.svg = this.svg
+			.attr("width", this.width + this.margin.left + this.margin.right)
+		    .attr("height", this.height + this.margin.top + this.margin.bottom)
+		  .append("g")
+		    .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+
+  		// Reescribe función generadora del mensaje en tooltip
+		this.tooltip.message = this.tootipMessage;
+
+
+		// Limpia Data
+		// ===========
+		// Limpia datos de entrada (P.Ej. Cambiar duración de semestres a años)
+		this.data = _.map(this.data, function(d) {
+			//d.desercionagno1 = parseFloat(d.desercionagno1.replace(/\.|%/g,'').replace(/,/g,'.')).toString();
+			d.duracion_real = d.duracion_real/2;  // Cambiar a años
+			d.costo_estimado = d.arancel*d.duracion_real; // Genera nuevo atributo de costo carrera
+			d.id = d.institucion+d.carrera; //Simula un id único al no venir en los datos
+			return d;
+		})
+
+		// Calcula Parámetros
+		// ==================
+		//Calcula parámetros que dependen de los datos
+
+		// Tipos de IE (Universidades, CFT, ...)
+		// Es utilizado para cambiar el color de cada nodo 
+		this.tiposIEs = _.unique(_.pluck(this.data, this.attrcolor)).sort();
+
+		// Arreglo con los tidos de IES que están seleccionados para mostrarsse de manera destacada
+		// originalmente se sólo la última opción
+		this.tiposSeleccionados = this.tiposIEs;  
+
+		// Obtiene un arreglo con los nombres de las áreas (Ej ["Salud", "Educación", ...])
+		this.areas = _.unique(_.pluck(this.data,this.attrfilter)).sort();
+		// Selecciona la primera área como areaSeleccionada
+		this.areaSeleccionada = this.areas.length >0 ? this.areas[0] : ""; 
+
+		// Filtra Data
+		// ===========
+		// Datos seleccionados que serán utilizados para graficar
+		// Originalmente filtro los correspondoientes a la primera area (this.areaSeleccionada)
+		this.filteredData = _.filter(this.data, function(d) {
+			return 	(d.area == self.areaSeleccionada) &&			// Carreras del área seleccionada
+					(parseFloat(d[self.attrx])>0) && 	// Atributo x es un número válido
+					(d[self.attry] != "s/i");			// Atributo y es categoría valida (no s/i)
+		});
+
+		// Genera escalas
+		// ==============
+		// Genera escala de color utilizada en el gráfico
+		this.colorScale = d3.scale.category10();
+		// El dominio corresponde a los tipos de IEs
+		this.colorScale.domain(this.tiposIEs);
+
+		// parametros de Ingreso: nombres (utilizado en eje Y); y piso (utilizado para calcular tiempo de break even)
+		this.parametrosIngreso= {
+			"Menor a $400 mil" : 200000,
+    		"De $400 mil a $600 mil" : 400000,
+			"De $600 mil a $800 mil" : 600000,
+			"De $800 mil a $1 millón" : 800000,
+			"De $1 millón a $1 millón 200 mil" : 1000000,
+			"De $1 millón 200 mil a $1 millón 400 mil" : 1200000,
+			"De $1 millón 400 mil a $1 millón 600 mil" : 1400000,
+			"De $1 millón 600 mil a $1 millón 800 mil" : 1600000,
+			"De $1 millón 800 mil a $2 millones" : 1800000,
+			"Sobre $2 millones" : 2000000};
+
+		// Arreglo con nombres de categorías de Ingreso
+		var domainIngreso = _.keys(this.parametrosIngreso);
+
+		// Escala Y
+		// Escala ordinal con dominio en  tramos de ingreso
+		this.yScale = d3.scale.ordinal()
+			.domain(domainIngreso)
+			.rangePoints([this.height, 0], 1);
+
+		// Escala X
+		// Escala lineal con dominio en attrX (costo estimado de carrera)
+		// Genera escalas utilizadas en gráfico X/Y
+		this.xScale = d3.scale.linear()
+    		.range([0, this.width])
+			.domain(d3.extent(this.data, function(d) { 
+				return parseFloat(d[self.attrx])
+			})).nice();
+
+
+		// Crea Ejes X e Y
+		// ===============
+		// Construye ejes X e Y
+		this.xAxis = d3.svg.axis()
+		    .scale(this.xScale)
+		    .orient("bottom");
+
+		this.yAxis = d3.svg.axis()
+		    .scale(this.yScale)
+		    .orient("left")
+
+		// Vista que dibuja ejes X e Y en elemento SVG
+ 		this.ejes = new VistaEjesXY({
+			svg: this.svg,
+			x:this.xScale, y:this.yScale, 
+			height: this.height, width: this.width, 
+			labelX: this.etiquetas[this.attrX],labelY: this.etiquetas[this.attrY]
+		})
+
+
+		this.render();
+ 
 	},
 
 	/**
@@ -90,15 +267,9 @@ var VistaPrincipal = Backbone.View.extend(
 	*/
 	seleccionTipoIE : function(tiposSeleccionados) {
 		this.tiposSeleccionados = tiposSeleccionados;
-		this.reDraw();
+		this.render();
 	},
 
-	// seleccionArea
-	// -------------
-	// Función invocada cuando se seleccione una nueva área
-	// gatillada al recibir evento de vista this.vistaSelectorAreas
-	// Parámetros:
-	// - areaSeleccionada: string con area seleccionada
 	
 	/**
 	* Selecciona una nueva área discipplinar (Ej. "Educación"). 
@@ -120,14 +291,29 @@ var VistaPrincipal = Backbone.View.extend(
 					(d[self.attry] != "s/i");			// Atributo y es categoría valida (no s/i)
 		});
 
-		this.reDraw();
+		this.render();
 	},
 
 	/**
-	* Función que dibuja los elementos del gráfico. 
-	* Es llamada cada vez que se hace un cambuo (p.ej. selección de una nueva área)
+	* Obtiene la escala de colores del gráfico.
+	* @return {d3.scale}
 	*/
-	reDraw: function() {
+	getColorScale : function() {
+		return this.colorScale;
+	},
+
+	/**
+	* Obtiene arreglo con el listado de áreas (disciplinas) de los datos en el gráfico
+	* @return {array}
+	*/
+	getAreas : function() {
+		return this.areas;
+	},
+
+	/**
+	* Despliegue inicial de elementos gráficos.
+	*/
+	render: function() {
 		var self = this; // Auxiliar para referirse a this en funciones callback
 
 		// Calcula el dominio de las escala x en base al valor de los ultimos datos seleccionados 
@@ -188,155 +374,14 @@ var VistaPrincipal = Backbone.View.extend(
 			.attr("opacity", function(d) {
 				// Verificar si el tipo de IE está cintenida en los tipos seleccionados
 				// y asignar valor de opacidad en concordancia
-				return _.contains(self.tiposSeleccionados, d[self.attrTipoIE]) ? 0.95 : 0.05
+				return _.contains(self.tiposSeleccionados, d[self.attrcolor]) ? 0.95 : 0.05
 			})
-
-	},
-
-	/**
-	* Despliegue inicial de elementos gráficos.
-	*/
-	render: function() {
-		var self = this; // Para hacer referencia a "this" en callback functions
-
-		// Limpia Data
-		// ===========
-		// Limpia datos de entrada (P.Ej. Cambiar duración de semestres a años)
-		this.data = _.map(this.data, function(d) {
-			//d.desercionagno1 = parseFloat(d.desercionagno1.replace(/\.|%/g,'').replace(/,/g,'.')).toString();
-			d.duracion_real = d.duracion_real/2;  // Cambiar a años
-			d.costo_estimado = d.arancel*d.duracion_real; // Genera nuevo atributo de costo carrera
-			d.id = d.institucion+d.carrera; //Simula un id único al no venir en los datos
-			return d;
-		})
-
-
-		// Calcula Parámetros
-		// ==================
-		//Calcula parámetros que dependen de los datos
-
-		// Tipos de IE (Universidades, CFT, ...)
-		// Es utilizado para cambiar el color de cada nodo 
-		this.tiposIEs = _.unique(_.pluck(this.data, this.attrTipoIE)).sort();
-
-		// Arreglo con los tidos de IES que están seleccionados para mostrarsse de manera destacada
-		// originalmente se sólo la última opción
-		this.tiposSeleccionados = this.tiposIEs;  
-
-		// Obtiene un arreglo con los nombres de las áreas (Ej ["Salud", "Educación", ...])
-		this.areas = _.unique(_.pluck(this.data,"area")).sort();
-		// Selecciona la primera área como areaSeleccionada
-		this.areaSeleccionada = this.areas.length >0 ? this.areas[0] : ""; 
-
-		// Filtra Data
-		// ===========
-		// Datos seleccionados que serán utilizados para graficar
-		// Originalmente filtro los correspondoientes a la primera area (this.areaSeleccionada)
-		this.filteredData = _.filter(this.data, function(d) {
-			return 	(d.area == self.areaSeleccionada) &&			// Carreras del área seleccionada
-					(parseFloat(d[self.attrx])>0) && 	// Atributo x es un número válido
-					(d[self.attry] != "s/i");			// Atributo y es categoría valida (no s/i)
-		});
-
-		// Genera escalas
-		// ==============
-		// Genera escala de color utilizada en el gráfico
-		this.colorScale = d3.scale.category10();
-		// El dominio corresponde a los tipos de IEs
-		this.colorScale.domain(this.tiposIEs);
-
-		// parametros de Ingreso: nombres (utilizado en eje Y); y piso (utilizado para calcular tiempo de break even)
-		this.parametrosIngreso= {
-			"Menor a $400 mil" : 200000,
-    		"De $400 mil a $600 mil" : 400000,
-			"De $600 mil a $800 mil" : 600000,
-			"De $800 mil a $1 millón" : 800000,
-			"De $1 millón a $1 millón 200 mil" : 1000000,
-			"De $1 millón 200 mil a $1 millón 400 mil" : 1200000,
-			"De $1 millón 400 mil a $1 millón 600 mil" : 1400000,
-			"De $1 millón 600 mil a $1 millón 800 mil" : 1600000,
-			"De $1 millón 800 mil a $2 millones" : 1800000,
-			"Sobre $2 millones" : 2000000};
-
-		// Arreglo con nombres de categorías de Ingreso
-		var domainIngreso = _.keys(this.parametrosIngreso);
-
-		// Escala Y
-		// Escala ordinal con dominio en  tramos de ingreso
-		this.yScale = d3.scale.ordinal()
-			.domain(domainIngreso)
-			.rangePoints([this.height, 0], 1);
-
-		// Escala X
-		// Escala lineal con dominio en attrX (costo estimado de carrera)
-		// Genera escalas utilizadas en gráfico X/Y
-		this.xScale = d3.scale.linear()
-    		.range([0, this.width])
-			.domain(d3.extent(this.data, function(d) { 
-				return parseFloat(d[self.attrx])
-			})).nice();
-
-		// Vistas auxiliares pre SVG
-		// =======================================
-		// Vista con tooltip para mostrar ficha de establecimiento
-		//this.tooltip = new VistaToolTipEstablecimiento();
-		this.tooltip = new VistaToolTip();
-		this.tooltip.message = this.tootipMessage;
-
-		// Cea un nuevo panel para seleccionar tipos de IEs ("Universidades", "Institutos Profesionales", ...)
-    	// -------------------------------------------------
-    	// Crea un nuevo div para contener el panel
-    	var elSelectorTipoIEs = d3.select(this.el).append("div").attr("id", "panelSelectorTipoIE")
-		// Genera contenido del panel y escucha a evento con tipo seleccionado
-		this.vistaSelectorTipoIE= new VistaPanelTipoIE({el:"#panelSelectorTipoIE", colorScale: this.colorScale});
-		this.vistaSelectorTipoIE.on("seleccionTipoIE", this.seleccionTipoIE);
-
-
-		// Crea un nuevo panel para seleccionar áreas
-		// -----------------------------------------
-		// Crea nuevo div que contendrá el panel
-		var elSelectorAreas = d3.select(this.el).append("div").attr("id", "panelSelectorAreas")
-		// Genera el contenido del panel y escucha a evento con area seleccionada
-		this.vistaSelectorAreas = new VistaPanelSelectorAreas({el: "#panelSelectorAreas", areas: this.areas})
-		this.vistaSelectorAreas.on("seleccionArea", this.seleccionArea);
-
-
-		// Elemento SVG (contenedor principal de gráfica)
-		// ==============================================
-		// Genera elemento SVG contenedor principal de gráficos
-		this.svg = d3.select(this.el).append("svg")
-		    .attr("width", this.width + this.margin.left + this.margin.right)
-		    .attr("height", this.height + this.margin.top + this.margin.bottom)
-		  .append("g")
-		    .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
-
-
-		// Crea Ejes X e Y
-		// ===============
-		// Construye ejes X e Y
-		this.xAxis = d3.svg.axis()
-		    .scale(this.xScale)
-		    .orient("bottom");
-
-		this.yAxis = d3.svg.axis()
-		    .scale(this.yScale)
-		    .orient("left")
-
-		// Vista que dibuja ejes X e Y en elemento SVG
- 		this.ejes = new VistaEjesXY({
-			svg: this.svg,
-			x:this.xScale, y:this.yScale, 
-			height: this.height, width: this.width, 
-			labelX: this.etiquetas[this.attrX],labelY: this.etiquetas[this.attrY]
-		})
-
- 		// Dibuja los elementos del gráfico
- 		// ================================
-		this.reDraw();
-
 	}
 
 });
+
+
+
 
 var VistaPanelTipoIE = Backbone.View.extend(
 /** @lends VistaPanelTipoIE.prototype */
@@ -414,6 +459,9 @@ var VistaPanelTipoIE = Backbone.View.extend(
 		return this;
 	}
 });
+
+
+
 
 
 var VistaPanelSelectorAreas = Backbone.View.extend(
